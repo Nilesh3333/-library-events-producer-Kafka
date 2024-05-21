6b21d367433bfcabf56a94b027f04f802995bd25
Nilesh <Nilesh.bommisetty@gmail.com>
2024-05-21 15:52:20 -0400

    Final Commit

---
+++ library-events-consumer/library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,56 @@
+package com.learnkafka.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.List;
+
+
+@Configuration
+@Slf4j
+//@EnableKafka
+public class LibraryEventsConsumerConfig {
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, (ConsumerFactory<Object, Object>) kafkaConsumerFactory);
+        factory.setConcurrency(3);  //for concurrent Listeners
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);  //To set the consumer offset to manual
+        factory.setCommonErrorHandler(errorHandler());  //For handling common error handlers
+        return factory;
+    }
+
+    public DefaultErrorHandler errorHandler() {
+
+        var exceptiopnToIgnorelist = List.of(
+                IllegalArgumentException.class
+        );
+
+        ExponentialBackOffWithMaxRetries expBackOff = new ExponentialBackOffWithMaxRetries(2);
+        expBackOff.setInitialInterval(1_000L);
+        expBackOff.setMultiplier(2.0);
+        expBackOff.setMaxInterval(2_000L);
+
+        var fixedBackOff = new FixedBackOff(1000L, 2L);
+
+        return new DefaultErrorHandler(fixedBackOff);
+    }
+
+
+
+}
